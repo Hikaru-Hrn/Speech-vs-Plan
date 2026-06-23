d38180cdefd72975ac3638b6fa208bd790c5ba4a
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-06-23 20:03:14 +0800

    edit: added definition of FastAPI object and imports

---
+++ src/parse_form.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel
+import json
+import os
+
+templates = Jinja2Templates(directory="templates")
+app = FastAPI()
+
