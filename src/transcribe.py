4acccbe8af6f49083090f5255b0241bce63d0627
Hikaru-Hrn <geniysenkou@gmail.com>
2026-06-26 17:39:05 +0800

    Added test files for transcribe

--- src/transcribe.py
+++ src/transcribe.py
@@ -4,5 +4,7 @@ import sys
 import grpc
 from cloudapi.output.transcribe import transcribe_audio_file
 
+
+# Тест вызова функции транскрибации с помощью Yandex SpeechKit
 if __name__ == '__main__':
     transcribe_audio_file("../data/inbox_audio_test/Запись.wav", "../data/outbox_audio_test/transcript.txt")
