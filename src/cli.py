9b18e752d63697ddf0bc7f1dadb6eee06619af9c
Hikaru-Hrn <geniysenkou@gmail.com>
2026-06-28 22:25:34 +0800

    Added a test function for writing tokens to .env

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,20 @@
 import typer
+from itsdangerous import encoding
 
 from transcribe import transcribe_audio_file
 
-def main(input: str, output:str):
+def main(tokens: bool = typer.Option(False, "--tokens", "-t", help="token mod"),
+         test: bool = typer.Option(False, "--fefe", "-f", help="test mode")):
     """
     Тест Yandex SpeechKit
     """
-    return transcribe_audio_file(input, output)
-
+    if tokens:
+        iam_token = input("Введите IAM токен: ")
+        folder_id = input("Введите FOLDER_ID: ")
+        with open("../.testenv", "w", encoding="utf-8") as f:
+            f.write(f"IAM_TOKEN={iam_token}")
+            f.write("\n")
+            f.write(f"FOLDER_ID={folder_id}")
 
 if __name__ == "__main__":
     typer.run(main)
