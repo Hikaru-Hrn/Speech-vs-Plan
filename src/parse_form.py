78890bb8698cfdaa44959811f7053aaf86b45f8d
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-06-30 21:36:49 +0800

    feature: added error handling

--- src/parse_form.py
+++ src/parse_form.py
@@ -73,108 +73,147 @@ async def step_two_page(request: Request):
 @app.get("/step3")
 async def step_three_page(request: Request):
     """Function to show page with comparing lecture and its plan"""
-    return templates.TemplateResponse(request=request, name="step_three.html")
+    try:
+        return templates.TemplateResponse(request=request, name="step_three.html")
+    except Exception as e:
+        return {"status": "fail", "error_msg": e}
 
 
 @app.get("/upload-plan")
 async def upload_plan(request: Request):
     """Function to show page where user can upload lecture plan file"""
-    return templates.TemplateResponse(request=request, name="upload_plan.html")
+    try:
+        return templates.TemplateResponse(request=request, name="upload_plan.html")
+    except Exception as e:
+        return {"status": "fail", "error_msg": e}
 
 
 @app.get("/lectures", response_class=HTMLResponse)
 async def show_form(request: Request):
     """Function to show form"""
-    return templates.TemplateResponse(request=request, name="form.html")
+    try:
+        return templates.TemplateResponse(request=request, name="form.html")
+    except Exception as e:
+        return {"status": "fail", "error_msg": e}
 
 @app.get("/upload-transcript")
 async def show_upload_transcript_page(request: Request):
     """Function to show page to upload prepared transcript"""
-    return templates.TemplateResponse(request=request, name="upload_audio.html")
+    try:
+        return templates.TemplateResponse(request=request, name="upload_audio.html")
+    except Exception as e:
+        return {"status": "fail", "error_msg": e}
 
 @app.post("/save-plan")
 async def save_uploaded_plan(file: UploadFile = File(...)):
     """Function to save uploaded plan file"""
 
-    save_file(destination_dir=UPLOADED_PLANS_DIR, file_type='',
-                   file=file)
-
-    return RedirectResponse(url="/plan-to-json")
+    try:
+        save_file(destination_dir=UPLOADED_PLANS_DIR, file_type='',
+                    file=file)
+        return RedirectResponse(url="/plan-to-json")
+    except Exception as e:
+        return {"status": "fail", "error_msg": e}
 
 
 @app.post("/plan-to-json")
 async def uploaded_file_to_json(request: Request):
     """Function to convert plans files to JSON"""
 
-    filename = os.listdir(UPLOADED_PLANS_DIR)[0]
-    if not os.path.isdir(LECTURES_JSONS_DIR):
-        os.mkdir(LECTURES_JSONS_DIR)
-    file_id = len(os.listdir(LECTURES_JSONS_DIR))
-    json_filename = f"lecture_{file_id}.json"
-    plan_to_json(f"{UPLOADED_PLANS_DIR}/{filename}", f"{LECTURES_JSONS_DIR}/{json_filename}")
-    file_id += 1
-    FILES_TO_PROCESS["lecture_plan"] = f"{LECTURES_JSONS_DIR}/{json_filename}"
-    for item in os.listdir(UPLOADED_PLANS_DIR):
-        file_path = os.path.join(UPLOADED_PLANS_DIR, item)
-        if os.path.isfile(file_path):
-            os.remove(file_path) 
-    return {
-                "status": "success", 
-                "redirect_url": "/step2"
-            }
+    try:
+        filename = os.listdir(UPLOADED_PLANS_DIR)[0]
+        if not os.path.isdir(LECTURES_JSONS_DIR):
+            os.mkdir(LECTURES_JSONS_DIR)
+        file_id = len(os.listdir(LECTURES_JSONS_DIR))
+        json_filename = f"lecture_{file_id}.json"
+        plan_to_json(f"{UPLOADED_PLANS_DIR}/{filename}", f"{LECTURES_JSONS_DIR}/{json_filename}")
+        file_id += 1
+        FILES_TO_PROCESS["lecture_plan"] = f"{LECTURES_JSONS_DIR}/{json_filename}"
+        for item in os.listdir(UPLOADED_PLANS_DIR):
+            file_path = os.path.join(UPLOADED_PLANS_DIR, item)
+            if os.path.isfile(file_path):
+                os.remove(file_path) 
+        return {
+                    "status": "success", 
+                    "redirect_url": "/step2",
+                    "error_msg": None
+                }
+    except Exception as e:
+        return {
+                    "status": "fail",
+                    "redirect_url": None,
+                    "error_msg": e
+                }
 
 @app.post("/lectures/add")
 async def lecture_to_json(stages: List[Stage]):
     """Function to save lecture"""
-    if not os.path.isdir(LECTURES_JSONS_DIR):
-        os.mkdir(LECTURES_JSONS_DIR)
-    stages_list = [stage.model_dump() for stage in stages]
-    file_name = f"{LECTURES_JSONS_DIR}/lecture_{len(os.listdir(LECTURES_JSONS_DIR))}.json"
-    lecture_json = {
-        "paragraphs": stages_list
-    }
-    with open(file_name, 'w', encoding='utf-8') as file:
-        json.dump(lecture_json, file, ensure_ascii=False)
-    
-    return {"status": "success", "message": "Лекция сохранена", "redirect_url": "/step2"}
+    try:
+        if not os.path.isdir(LECTURES_JSONS_DIR):
+            os.mkdir(LECTURES_JSONS_DIR)
+        stages_list = [stage.model_dump() for stage in stages]
+        file_name = f"{LECTURES_JSONS_DIR}/lecture_{len(os.listdir(LECTURES_JSONS_DIR))}.json"
+        lecture_json = {
+            "paragraphs": stages_list
+        }
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(lecture_json, file, ensure_ascii=False)
+        return {
+            "status": "success", 
+            "message": "Лекция сохранена", 
+            "redirect_url": "/step2",
+            "error_msg": None
+            }
+    except Exception as e:
+        return {
+            "status": "fail",
+            "message": "Ошибка при сохранении лекции",
+            "redirect_url": "/step2",
+            "error_msg": e
+        }
 
 @app.post("/save-audiofile")
 async def save_uploaded_audiofile(file: UploadFile = File(...)):
     """Function to save uploaded audiofile"""
-
-    file_path = save_file(destination_dir=UPLOADED_AUDIOFILES_DIR, file_type="audiofile",
-               file=file)
-    FILES_TO_PROCESS["lecture_audiofile"] = file_path
-    
-    return {"status": "success", "message": "audiofile uploaded"}
+    try:
+        file_path = save_file(destination_dir=UPLOADED_AUDIOFILES_DIR, file_type="audiofile",
+                file=file)
+        FILES_TO_PROCESS["lecture_audiofile"] = file_path
+        
+        return {"status": "success", "message": "audiofile uploaded", "error_msg": None}
+    except Exception as e:
+        return {"status": "fail", "message": "an unexcpected", "error_msg": e}
 
 @app.post("/speech-transcribe-request")
 async def transcribe_api_request(requst: Request):
-    if not os.path.isdir(TRANSCRIPTS_DIR):
-        os.mkdir(TRANSCRIPTS_DIR)
-    file_id = len(os.listdir(TRANSCRIPTS_DIR))
-    lecture_transcribe_path = f"{TRANSCRIPTS_DIR}/lecture_{file_id}_transcript.txt"
-    is_processed = transcribe_audio_file(input_audio_path=FILES_TO_PROCESS["lecture_audiofile"],
-                                         output_text_path=lecture_transcribe_path)
-    FILES_TO_PROCESS["lecture_transcript"] = lecture_transcribe_path
-    print(is_processed)
-    if is_processed:
-        return {"message": "success", "redirect_url": "/step3"}
-    else:
-        return {"message": "fail", "redirect_url": None}
+    """Function to transcribe audiofile"""
+    try:
+        if not os.path.isdir(TRANSCRIPTS_DIR):
+            os.mkdir(TRANSCRIPTS_DIR)
+        file_id = len(os.listdir(TRANSCRIPTS_DIR))
+        lecture_transcribe_path = f"{TRANSCRIPTS_DIR}/lecture_{file_id}_transcript.txt"
+        transcribe_audio_file(input_audio_path=FILES_TO_PROCESS["lecture_audiofile"],
+                                            output_text_path=lecture_transcribe_path)
+        FILES_TO_PROCESS["lecture_transcript"] = lecture_transcribe_path
+        return {"message": "success", "redirect_url": "/step3", "error_msg": None}
+    except Exception as e:
+        return {"message": "fail", "redirect_url": None, "error_msg": e}
 
 @app.post("/gigachat-api-request")
 async def gigachat_api_request():
-    response = ask_with_file_content(FILES_TO_PROCESS["lecture_plan"], 
-                                     FILES_TO_PROCESS["lecture_transcript"])
-    if not os.path.isdir(COMPARISON_JSONS_DIR):
-        os.mkdir(COMPARISON_JSONS_DIR)
-    base_name = Path(FILES_TO_PROCESS['lecture_plan']).stem
-    file_name = f"{COMPARISON_JSONS_DIR}/{base_name}_comparison.json"
-    with open(file_name, 'w', encoding='utf-8') as json_file:
-        json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
-    FILES_TO_PROCESS['lecture_comparison'] = file_name
-    return {"message": "success", "gigachat_response_saved_to": file_name}
+    try:
+        response = ask_with_file_content(FILES_TO_PROCESS["lecture_plan"], 
+                                        FILES_TO_PROCESS["lecture_transcript"])
+        if not os.path.isdir(COMPARISON_JSONS_DIR):
+            os.mkdir(COMPARISON_JSONS_DIR)
+        base_name = Path(FILES_TO_PROCESS['lecture_plan']).stem
+        file_name = f"{COMPARISON_JSONS_DIR}/{base_name}_comparison.json"
+        with open(file_name, 'w', encoding='utf-8') as json_file:
+            json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
+        FILES_TO_PROCESS['lecture_comparison'] = file_name
+        return {"message": "success", "gigachat_response_saved_to": file_name, "error_msg": None}
+    except Exception as e:
+        return {"message": "fail", "gigachat_response_saved_to": None, "error_msg": e}
 
 @app.post("/save-transcript")
 async def upload_transcript(file: UploadFile = File(...)):
